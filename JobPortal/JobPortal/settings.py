fe648e3a09acb55cbc3e83f8b2b79c99cf17cd7a
Mugooli-Martin <martinlanastar@gmail.com>
2025-06-27 20:33:57 +0300

    Applied basic css styles to register forms

--- JobPortal/JobPortal/settings.py
+++ JobPortal/JobPortal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h^ok0235g8wwnm(xs4%jmivhcosx3#=2eq(ok6#u-0n#l74mbf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'JobPortal.urls'
@@ -118,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR /'staticfiles'
 STATIC_URL = 'static/'
 
 # Default primary key field type
